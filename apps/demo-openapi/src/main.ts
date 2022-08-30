3153c891fc159ab96c2669a34db03ce7ad450904
Niclas Haderer <niclas.haderer.dev@gmail.com>
2022-08-30 13:55:42 +0200

    Fixed example

--- apps/demo-openapi/src/main.ts
+++ apps/demo-openapi/src/main.ts
@@ -2,7 +2,7 @@ import { apiServer, swaggerRouter } from "@luftschloss/openapi"
 import { luft } from "@luftschloss/validation"
 
 const main = async () => {
-  const server = apiServer()
+  const server = apiServer({ openApi: { info: { title: "Hello world", version: "0.0.0" } } })
   server
     .get("", {
       query: luft.object({
@@ -11,9 +11,11 @@ const main = async () => {
       response: luft.object({
         hello: luft.string(),
       }),
+      headers: undefined,
+      path: undefined,
     })
-    .handle(url => {
-      return url
+    .handle(({ query }) => {
+      return query
     })
   server.mount(swaggerRouter())
   server.listen()
