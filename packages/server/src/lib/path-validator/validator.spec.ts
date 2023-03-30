1b2a6bb128de7d1a2e9d0f8ecff12a1a2e7c6505
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-03-30 18:49:50 +0200

    test: more regex tests

--- packages/server/src/lib/path-validator/validator.spec.ts
+++ packages/server/src/lib/path-validator/validator.spec.ts
@@ -1,4 +1,11 @@
-import { containsRegex, DEFAULT_PATH_VALIDATOR_NAME, pathToRegex, stringPathValidator } from ".";
+import {
+  containsRegex,
+  DEFAULT_PATH_VALIDATOR_NAME,
+  intPathValidator,
+  numberPathValidator,
+  pathToRegex,
+  stringPathValidator,
+} from ".";
 
 test("Contains: default parameter string", () => {
   const path1 = "hello/{world}";
@@ -20,30 +27,96 @@ test("Contains: explicit parameter string", () => {
 
 test("Contains: explicit no variable name", () => {
   const path1 = "hello/{:int}";
-  const path2 = "/hello/{:number}";
-  const path3 = "/hello/{:string}/";
-  const path4 = "/hello/{a:}";
   expect(containsRegex(path1)).toBe(true);
+
+  const path2 = "/hello/{:number}";
   expect(containsRegex(path2)).toBe(true);
+
+  const path3 = "/hello/{:string}/";
   expect(containsRegex(path3)).toBe(true);
+
+  const path4 = "/hello/{a:}";
   expect(containsRegex(path4)).toBe(true);
 });
 
 test("Contains: invalid", () => {
   const path1 = "/hello/{}/";
-  const path2 = "/hello/{:}";
   expect(containsRegex(path1)).toBe(false);
+
+  const path2 = "/hello/{:}";
   expect(containsRegex(path2)).toBe(false);
 });
 
-test("Convert to path", () => {
+test("Convert: default parameter string", () => {
   const validators = { string: stringPathValidator(), [DEFAULT_PATH_VALIDATOR_NAME]: stringPathValidator() };
+
+  const path1 = "hello/{world}";
+  const regex1 = pathToRegex(path1, validators);
+  expect(regex1).toEqual(/^\/hello\/(?<world>[^/]+)\/$/i);
+
+  const path2 = "/hello/{world}";
+  const regex2 = pathToRegex(path2, validators);
+  expect(regex2).toEqual(/^\/hello\/(?<world>[^/]+)\/$/i);
+
+  const path3 = "/hello/{world}/";
+  const regex3 = pathToRegex(path3, validators);
+  expect(regex3).toEqual(/^\/hello\/(?<world>[^/]+)\/$/i);
+});
+
+test("Convert: explicit parameter string", () => {
+  const validators = {
+    string: stringPathValidator(),
+    int: intPathValidator(),
+    number: numberPathValidator(),
+    [DEFAULT_PATH_VALIDATOR_NAME]: stringPathValidator(),
+  };
+
+  const path1 = "hello/{world:int}";
+  const regex1 = pathToRegex(path1, validators);
+  expect(regex1).toEqual(/^\/hello\/(?<world>[+-]?\d+)\/$/i);
+
+  const path2 = "/hello/{world:number}";
+  const regex2 = pathToRegex(path2, validators);
+  expect(regex2).toEqual(/^\/hello\/(?<world>[+-]?(?:[0-9]*\.)?[0-9]+)\/$/i);
+
+  const path3 = "/hello/{world:string}/";
+  const regex3 = pathToRegex(path3, validators);
+  expect(regex3).toEqual(/^\/hello\/(?<world>[^/]+)\/$/i);
+});
+
+test("Convert: explicit no variable name", () => {
+  const validators = {
+    string: stringPathValidator(),
+    int: intPathValidator(),
+    number: numberPathValidator(),
+    [DEFAULT_PATH_VALIDATOR_NAME]: stringPathValidator(),
+  };
+
+  const path1 = "hello/{:int}";
+  const regex1 = pathToRegex(path1, validators);
+  expect(regex1).toEqual(/^\/hello\/(?:[+-]?\d+)\/$/i);
+
+  const path2 = "/hello/{:number}";
+  const regex2 = pathToRegex(path2, validators);
+  expect(regex2).toEqual(/^\/hello\/(?:[+-]?(?:[0-9]*\.)?[0-9]+)\/$/i);
+
+  const path3 = "/hello/{:string}/";
+  const regex3 = pathToRegex(path3, validators);
+  expect(regex3).toEqual(/^\/hello\/(?:[^/]+)\/$/i);
+});
+
+test("Convert: To path", () => {
+  const validators = { string: stringPathValidator(), [DEFAULT_PATH_VALIDATOR_NAME]: stringPathValidator() };
+
   const pathRegex1 = pathToRegex("/hello/{a:string}", validators);
-  const pathRegex2 = pathToRegex("/hello/{:string}/world", validators);
-  const pathRegex3 = pathToRegex("/hello/{a}/world", validators);
-  const pathRegex4 = pathToRegex("/hello/{a:}/world", validators);
   expect(pathRegex1).toEqual(/^\/hello\/(?<a>[^/]+)\/$/i);
+
+  const pathRegex2 = pathToRegex("/hello/{:string}/world", validators);
   expect(pathRegex2).toEqual(/^\/hello\/(?:[^/]+)\/world\/$/i);
+
+  const pathRegex3 = pathToRegex("/hello/{a}/world", validators);
   expect(pathRegex3).toEqual(/^\/hello\/(?<a>[^/]+)\/world\/$/i);
+
+  const pathRegex4 = pathToRegex("/hello/{a:}/world", validators);
   expect(pathRegex4).toEqual(/^\/hello\/(?<a>[^/]+)\/world\/$/i);
 });
