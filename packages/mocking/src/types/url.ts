6a3f4bd7842dfc667e4a55becb925fbfdeeb4e1c
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-04-04 11:38:50 +0200

    fix: removed redundant variable

--- packages/mocking/src/types/url.ts
+++ packages/mocking/src/types/url.ts
@@ -1,7 +1,7 @@
 import { LuftURL } from "@luftschloss/validation";
 import { faker } from "@faker-js/faker";
 
-export const mockURL = <T extends LuftURL>(validator: T, filedName?: string): URL => {
+export const mockURL = <T extends LuftURL>(validator: T): URL => {
   const url = new URL(faker.internet.url());
   if (validator.schema.protocols && !(url.protocol in validator.schema.protocols)) {
     url.protocol = validator.schema.protocols[0];
