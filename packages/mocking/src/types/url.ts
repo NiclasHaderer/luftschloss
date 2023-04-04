554b616bc13be215e0bebd3bb1448f56bfadd1fb
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-04-04 11:38:01 +0200

    fix: fixed protocols for mocked URLs

--- packages/mocking/src/types/url.ts
+++ packages/mocking/src/types/url.ts
@@ -2,5 +2,10 @@ import { LuftURL } from "@luftschloss/validation";
 import { faker } from "@faker-js/faker";
 
 export const mockURL = <T extends LuftURL>(validator: T, filedName?: string): URL => {
-  return new URL(faker.internet.url());
+  const url = new URL(faker.internet.url());
+  if (validator.schema.protocols && !(url.protocol in validator.schema.protocols)) {
+    url.protocol = validator.schema.protocols[0];
+  }
+
+  return url;
 };
