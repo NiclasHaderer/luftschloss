35aabcc96a8c267ec1d9f4588df3003b85f55786
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-09-02 17:01:47 +0200

    fix: types

--- packages/openapi-schema/src/lib/generators/lazy.ts
+++ packages/openapi-schema/src/lib/generators/lazy.ts
@@ -1,5 +1,5 @@
 import { LuftLazy } from "@luftschloss/validation";
 import { generateJsonSchema } from "./all";
 
-export const generateLazyJsonSchema = (type: LuftLazy, schemaPath: string) =>
+export const generateLazyJsonSchema = (type: LuftLazy<any>, schemaPath: string) =>
   generateJsonSchema(type.schema.typeFactory(), schemaPath);
--- packages/validation-interfaces/src/lib/generators/lazy.ts
+++ packages/validation-interfaces/src/lib/generators/lazy.ts
@@ -1,5 +1,5 @@
 import { GenerateSubType, LuftInterface } from "./all";
 import { LuftLazy } from "@luftschloss/validation";
 
-export const generateInterfaceForLazy = (type: LuftLazy, generateSubType: GenerateSubType): LuftInterface =>
+export const generateInterfaceForLazy = (type: LuftLazy<any>, generateSubType: GenerateSubType): LuftInterface =>
   generateSubType(type.schema.typeFactory());
